24e763b24367cec2204de5317c1dc8a68fe56116
전기범 <37178265+JunKiBeom@users.noreply.github.com>
2024-03-30 18:58:57 +0900

    [level 0] Title: 세균 증식, Time: 0.05 ms, Memory: 65.6 MB -BaekjoonHub

--- 프로그래머스/0/120910. 세균 증식/세균 증식.java
+++ 프로그래머스/0/120910. 세균 증식/세균 증식.java
@@ -1,8 +1,6 @@
-import static java.lang.Math.pow;
-
 class Solution {
     public int solution(int n, int t) {
         int answer = 0;
-        return n * (int) pow(2,t);
+        return n * (int) Math.pow(2,t);
     }
 }

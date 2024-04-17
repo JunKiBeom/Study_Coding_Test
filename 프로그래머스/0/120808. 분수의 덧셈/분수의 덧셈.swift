0a51bdcc195dc0b28940999b6ca7c6743afe56cf
전기범 <37178265+JunKiBeom@users.noreply.github.com>
2024-04-17 22:10:58 +0900

    [level 0] Title: 분수의 덧셈, Time: 1.07 ms, Memory: 16.2 MB -BaekjoonHub

--- 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.swift
+++ 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.swift
@@ -1,13 +1,13 @@
 import Foundation
 
 func solution(_ numer1:Int, _ denom1:Int, _ numer2:Int, _ denom2:Int) -> [Int] {
-    var num3 = (numer1 * denom2) + (numer2 * denom1);
-    var den3 = denom1 * denom2;
+    var num3 = (numer1 * denom2) + (numer2 * denom1)
+    var den3 = denom1 * denom2
     
     for i in stride(from: num3, to: 0, by: -1) {
         if (num3 % i == 0 && den3 % i == 0) {
-            num3 /= i;
-            den3 /= i;
+            num3 /= i
+            den3 /= i
         }
     }
     return [num3, den3]

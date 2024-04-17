edd9741246301cafdb72ee2b207225d59b5ac717
전기범 <37178265+JunKiBeom@users.noreply.github.com>
2024-04-17 23:44:55 +0900

    [level 0] Title: 이어 붙인 수, Time: 4.61 ms, Memory: 83 MB -BaekjoonHub

---
+++ 프로그래머스/0/181928. 이어 붙인 수/이어 붙인 수.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+class Solution {
+    public int solution(int[] num_list) {
+        int answer = 0;
+        String even = Arrays.stream(num_list).filter(i -> i % 2 == 0).mapToObj(String::valueOf).collect(Collectors.joining());
+        String odd = Arrays.stream(num_list).filter(i -> i % 2 != 0).mapToObj(String::valueOf).collect(Collectors.joining());
+
+        answer = Integer.parseInt(even) + Integer.parseInt(odd);
+        return answer;
+    }
+}

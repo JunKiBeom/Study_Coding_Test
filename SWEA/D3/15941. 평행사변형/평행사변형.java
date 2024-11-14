f55c090109a3e9183cbfcab1a645c9111efde99d
전기범 <37178265+JunKiBeom@users.noreply.github.com>
2024-11-15 03:47:00 +0900

    [D3] Title: 평행사변형, Time: 135 ms, Memory: 20,552 KB -BaekjoonHub

---
+++ SWEA/D3/15941. 평행사변형/평행사변형.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+import java.io.FileInputStream;
+
+class Solution
+{
+    public static void main(String args[]) throws Exception
+    {
+        Scanner sc = new Scanner(System.in);
+        int T;
+        T=sc.nextInt();
+
+        // 첫 번째 엔터 키를 소비하는 부분 추가
+        sc.nextLine();
+
+        for(int test_case = 1; test_case <= T; test_case++){
+            int n = sc.nextInt();
+            System.out.println("#"+test_case+" "+n*n);
+        }
+    }
+}

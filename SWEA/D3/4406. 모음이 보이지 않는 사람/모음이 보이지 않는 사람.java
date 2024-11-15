5b14aa7a2c23be8c6b736672480beeac545b718f
전기범 <37178265+JunKiBeom@users.noreply.github.com>
2024-11-16 01:12:36 +0900

    [D3] Title: 모음이 보이지 않는 사람, Time: 139 ms, Memory: 20,124 KB -BaekjoonHub

---
+++ SWEA/D3/4406. 모음이 보이지 않는 사람/모음이 보이지 않는 사람.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+import java.io.FileInputStream;
+
+class Solution
+{
+    public static void main(String args[]) throws Exception
+    {
+        Scanner sc = new Scanner(System.in);
+        int T;
+        T=sc.nextInt();
+
+        // 첫 번째 엔터 키를 소비하는 부분 추가
+        sc.nextLine();
+
+        for(int test_case = 1; test_case <= T; test_case++){
+            String str = sc.nextLine();
+            String ans = str.replaceAll("[aeiou]", "");
+
+            System.out.println("#"+test_case+" "+ans);
+        }
+    }
+}

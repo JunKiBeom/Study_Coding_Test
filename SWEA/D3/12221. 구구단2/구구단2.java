bda86fa8a5b26dcb103d2c51e1d78e81b6157b51
전기범 <37178265+JunKiBeom@users.noreply.github.com>
2024-11-15 23:55:34 +0900

    [D3] Title: 구구단2, Time: 135 ms, Memory: 20,344 KB -BaekjoonHub

---
+++ SWEA/D3/12221. 구구단2/구구단2.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+import java.io.FileInputStream;
+
+class Solution
+{
+    public static void main(String args[]) throws Exception
+    {
+        Scanner sc = new Scanner(System.in);
+        int T;
+        T=sc.nextInt();
+
+        // 첫 번째 엔터 키를 소비하는 부분 추가
+        sc.nextLine();
+
+        for(int test_case = 1; test_case <= T; test_case++){
+            int A = sc.nextInt();
+            int B = sc.nextInt();
+            
+            int ans = A>=10 ? -1 : B>=10 ? -1 : A*B;
+            System.out.println("#"+test_case+" "+ans);
+        }
+    }
+}

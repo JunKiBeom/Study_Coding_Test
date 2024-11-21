4f8bd79beb2a84c4e3a16b0b87ae75bb34639527
전기범 <37178265+JunKiBeom@users.noreply.github.com>
2024-11-22 01:51:28 +0900

    [Bronze III] Title: 학점계산, Time: 168 ms, Memory: 17636 KB -BaekjoonHub

--- 백준/Bronze/2754. 학점계산/학점계산.java
+++ 백준/Bronze/2754. 학점계산/학점계산.java
@@ -1,26 +1,28 @@
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String grade = sc.next();
-        double gpa = 0;
 
-        switch (grade) {
-            case "A+": gpa = 4.3; break;
-            case "A0": gpa = 4.0; break;
-            case "A-": gpa = 3.7; break;
-            case "B+": gpa = 3.3; break;
-            case "B0": gpa = 3.0; break;
-            case "B-": gpa = 2.7; break;
-            case "C+": gpa = 2.3; break;
-            case "C0": gpa = 2.0; break;
-            case "C-": gpa = 1.7; break;
-            case "D+": gpa = 1.3; break;
-            case "D0": gpa = 1.0; break;
-            case "D-": gpa = 0.7; break;
-            case "F":  gpa = 0.0; break;
-        }
-        System.out.printf("%.1f\n", gpa);
+        Map<String, Double> gradeMap = new HashMap<>();
+        gradeMap.put("A+", 4.3);
+        gradeMap.put("A0", 4.0);
+        gradeMap.put("A-", 3.7);
+        gradeMap.put("B+", 3.3);
+        gradeMap.put("B0", 3.0);
+        gradeMap.put("B-", 2.7);
+        gradeMap.put("C+", 2.3);
+        gradeMap.put("C0", 2.0);
+        gradeMap.put("C-", 1.7);
+        gradeMap.put("D+", 1.3);
+        gradeMap.put("D0", 1.0);
+        gradeMap.put("D-", 0.7);
+        gradeMap.put("F", 0.0);
+
+        String grade = sc.next();
+
+        System.out.println(gradeMap.get(grade));
     }
 }

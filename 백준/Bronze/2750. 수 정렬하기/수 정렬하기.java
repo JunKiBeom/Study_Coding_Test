bdaf0f0225ce812390d3ffa3fc88ac7144625395
전기범 <37178265+JunKiBeom@users.noreply.github.com>
2024-11-25 02:56:17 +0900

    [Bronze II] Title: 수 정렬하기, Time: 136 ms, Memory: 16228 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
@@ -2,14 +2,7 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int fact(int n) {
-        if(n == 0) {
-            return 1;
-        }
-        return n * fact(n - 1);
-    }
-
-    public static void main(String[] args) throws IOException {
+ public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());

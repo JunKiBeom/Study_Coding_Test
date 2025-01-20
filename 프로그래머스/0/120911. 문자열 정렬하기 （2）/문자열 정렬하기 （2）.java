0b69036903cfba95193981462b21282e988e430a
전기범 <37178265+JunKiBeom@users.noreply.github.com>
2025-01-20 17:50:37 +0900

    Update 문자열 정렬하기 （2）.java

--- 프로그래머스/0/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.java
+++ 프로그래머스/0/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.java
@@ -1,6 +1,10 @@
+import java.util.Arrays;
+
 class Solution {
     public String solution(String my_string) {
         String answer = "";
-        return answer;
+        char[] tmp = my_string.toLowerCase().toCharArray();
+        Arrays.sort(tmp);
+        return new String(tmp);
     }
 }

863216b5a6662ee8d7bb087a3057ce08da6642cf
전기범 <37178265+JunKiBeom@users.noreply.github.com>
2026-08-10 22:24:48 +0900

    [level 0] Title: 글자 지우기, Time: 0.27 ms, Memory: 78.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181900. 글자 지우기/글자 지우기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public String solution(String my_string, int[] indices) {
+        String[] arr = my_string.split("");
+        
+        for (int i: indices) {
+            arr[i] = "";
+        }
+        
+        return String.join("", arr);
+    }
+}

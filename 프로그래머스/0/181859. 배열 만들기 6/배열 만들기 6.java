291393969123ca9a838bc1a49e9248a417354c72
전기범 <37178265+JunKiBeom@users.noreply.github.com>
2026-09-15 03:08:27 +0900

    [level 0] Title: 배열 만들기 6, Time: 56.17 ms, Memory: 118 MB -BaekjoonHub

---
+++ 프로그래머스/0/181859. 배열 만들기 6/배열 만들기 6.java
@@ -0,0 +1,30 @@
+import java.util.Stack;
+
+class Solution {
+    public int[] solution(int[] arr) {
+        Stack<Integer> st = new Stack<>();
+        
+        for (int i = 0; i < arr.length; i++) {
+            if (st.isEmpty()) {
+                st.push(arr[i]);
+            }
+            else if (st.peek() == arr[i]) {
+                st.pop();
+            }
+            else {
+                st.push(arr[i]);
+            }
+        }
+        
+        if (st.isEmpty()) {
+            return new int[]{-1};
+        }
+        
+        int[] answer = new int[st.size()];
+        for (int i = st.size() - 1; i >= 0; i--) {
+            answer[i] = st.pop();
+        }
+        
+        return answer;
+    }
+}
